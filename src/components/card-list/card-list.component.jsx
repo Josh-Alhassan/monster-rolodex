f2e8c5cbff12e9d7463b77488cb07a41f969f18a
Alhassan Joshua Abel <saintjosh26@gmail.com>
2023-03-05 21:46:33 +0000

    refactored card-list component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,17 +1,25 @@
 import { Component } from "react";
-import "./card-list.styles.css";
-import Card from '../card/card.component';
+import './card-list.styles.css'
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
+
     return (
       <div className="card-list">
         {monsters.map((monster) => {
+          const { name, email, id } = monster;
 
           return (
-            <Card monster={monster} />
-          ); 
+            <div className="card-container" key={id}>
+              <img
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                alt={`monster ${name}`}
+              />
+              <h2>{name}</h2>
+              <p>{ email}</p>
+            </div>
+          );
         })}
       </div>
     );
@@ -19,3 +27,32 @@ class CardList extends Component {
 }
 
 export default CardList;
+// // import Card from "../card/card.component";
+// import "./card-list.styles.css";
+
+// class CardList extends Component {
+//   render() {
+//     const { monsters } = this.props;
+
+//     return (
+//       <div className="card-list" >
+//         {monsters.map((monster) => {
+//           const { name, email, id } = monster;
+
+//           return (
+//             <div className="card-container" key={id}>
+//             <img
+//               src={`https://robohash.org/${id}?set=set2&size=180x180`}
+//               alt={`monster ${name}`}
+//             />
+//             <h2>{name}</h2>
+//             <p>{email}</p>
+//           </div>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+// export default CardList;
